a335f2cf6b129fa8a88e02bb22a5f3fa030b780f
Yuehua00 <113094262+Yuehua00@users.noreply.github.com>
2024-02-15 22:14:32 +0800

    Create 12428 - Enemy At The Gates*.cpp

---
+++ 12428 - Enemy At The Gates*.cpp
@@ -0,0 +1,32 @@
+// Greedy
+#include <iostream>
+
+using namespace std;
+
+typedef unsigned long long int ull;
+typedef long long int ll;
+
+int main()
+{
+    int time = 0;
+    scanf("%d", &time);
+    while(time--){
+        int v = 0, n = 0;
+        scanf("%d%d", &n, &v);
+
+        if(n > v){
+            printf("%d\n", v);
+        }else{
+            for(ll i = 1; i <= n; i++){
+                ll rest = n-i;
+                if(i*(i-1)/2 >= v-rest){
+                    printf("%d\n", rest);
+                    break;
+                }
+            }
+        }
+    }
+
+    return 0;
+
+}

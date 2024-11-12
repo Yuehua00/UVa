1ee7d06c28935b3d55dbcb4f68ab1970d06c4515
Slider <113094262+Yuehua00@users.noreply.github.com>
2024-11-12 20:05:34 +0800

    Update 00714 - Copying Books.cpp

--- Binary Search/00714 - Copying Books.cpp
+++ Binary Search/00714 - Copying Books.cpp
@@ -6,22 +6,24 @@ using namespace std;
 
 long long Min = 0, Max = 0, mid = 0;
 int n = 0, m = 0, k = 0;
-int book[MAXN], tmp[MAXN];
-int ans[MAXN][MAXN];
+int book[MAXN];
+vector<int> ans[MAXN];
 
 int main()
 {
-    freopen("in.txt", "r", stdin);
-    freopen("out.txt", "w", stdout);
+    //freopen("in.txt", "r", stdin);
+    //freopen("out.txt", "w", stdout);
     scanf("%d", &n);
     while(n--){
+        for(int i = 0; i < MAXN; i++) ans[i].clear();
         Min = 0; Max = 0;
         scanf("%d%d", &m, &k);
         for(int i = 0; i < m; i++){
             scanf("%d", &book[i]);
-            if(book[i] > Min) Min = book[i];
+            if(book[i] > Min) Min = book[i];  // 紀錄書本中最大，可負荷量介於 書本最大 與 書本總和之間
             Max += book[i];
         }
+        // 二分搜，找最大每人可負荷量
         while(Min < Max){
             int cnt = 1;
             long long sum = 0;
@@ -39,21 +41,21 @@ int main()
         memset(tmp, 0, sizeof(tmp));
         long long sum = 0;
         for(int i = m-1, j = k-1; i >= 0; i--){
-            if(sum + book[i] > Max || j > i){
+            if(sum + book[i] > Max || j > i){     // 超過可讀頁數 || 都給他有人會沒拿到
                 j--;
                 sum = 0;
             }
             sum += book[i];
-            ans[j][tmp[j]++] = book[i];
+            ans[j].emplace_back(book[i]);
         }
-        for (int i = 0; i < k; i++){
-            for (int j=tmp[i]-1; j>=0; j--){
-                if (i!=0 || j!=tmp[0]-1) printf(" ");
-                printf("%d",ans[i][j]);
+        for(int i = 0; i < k; i++){
+            for(int j = ans[i].size()-1; j >= 0; j--){
+                if(i || j < ans[i].size()-1) printf(" ");
+                printf("%d", ans[i][j]);
             }
-            if (i != k-1) printf(" /");
+            if(i != k-1) printf(" /");
         }
-        printf("\n");
+        puts("");
 
     }
 
